9eaf7254d840c426c3d7565ac16ad64c9551d82f
Catlinman <andrew.lake@amwasser.de>
2014-11-06 20:37:16 +0100

    Improved formatting

--- canvas.input.js
+++ canvas.input.js
@@ -2,14 +2,14 @@ var input = {};
 input.focus = true;
 
 /*
-	88b           d88                                                   
-	888b         d888                                                   
-	88`8b       d8'88                                                   
-	88 `8b     d8' 88   ,adPPYba,   88       88  ,adPPYba,   ,adPPYba,  
-	88  `8b   d8'  88  a8"     "8a  88       88  I8[    ""  a8P_____88  
-	88   `8b d8'   88  8b       d8  88       88   `"Y8ba,   8PP"""""""  
-	88    `888'    88  "8a,   ,a8"  "8a,   ,a88  aa    ]8I  "8b,   ,aa  
-	88     `8'     88   `"YbbdP"'    `"YbbdP'Y8  `"YbbdP"'   `"Ybbd8"'  
+	88b           d88
+	888b         d888
+	88`8b       d8'88
+	88 `8b     d8' 88   ,adPPYba,   88       88  ,adPPYba,   ,adPPYba,
+	88  `8b   d8'  88  a8"     "8a  88       88  I8[    ""  a8P_____88
+	88   `8b d8'   88  8b       d8  88       88   `"Y8ba,   8PP"""""""
+	88    `888'    88  "8a,   ,a8"  "8a,   ,a88  aa    ]8I  "8b,   ,aa
+	88     `8'     88   `"YbbdP"'    `"YbbdP'Y8  `"YbbdP"'   `"Ybbd8"'
 */
 
 input.mouse = {};
@@ -26,8 +26,8 @@ input.mouse.keygroup = {};
 input.mouse.moveThisFrame = false;
 input.mouse.debug = false;
 
-input.mouse.addKey = function(name, keycode){
-	if(!input.mouse.keygroup[name]){
+input.mouse.addKey = function(name, keycode) {
+	if(!input.mouse.keygroup[name]) {
 		var mouseKey = [];
 		mouseKey.name = name;
 		mouseKey.keycode = keycode;
@@ -36,119 +36,125 @@ input.mouse.addKey = function(name, keycode){
 	}
 }
 
-input.mouse.popKey = function(name){
-	if(input.mouse.keygroup[name]){
+input.mouse.popKey = function(name) {
+	if(input.mouse.keygroup[name]) {
 		input.mouse.keygroup[name] = null;
 	}
 }
 
-input.mouse.press = function(x, y, keycode){
+input.mouse.press = function(x, y, keycode) {
 	input.mouse.x = x;
 	input.mouse.y = y;
 	input.mouse.lastKeycode = keycode;
 
-	for(var key in input.mouse.keygroup){
-		if(keycode == input.mouse.keygroup[key].keycode){
+	for(var key in input.mouse.keygroup) {
+		if(keycode == input.mouse.keygroup[key].keycode) {
 			input.mouse.keygroup[key].pressed = true;
 			return
 		}
 	}
 }
 
-input.mouse.release = function(){
-	for(var key in input.mouse.keygroup){
+input.mouse.release = function() {
+	for(var key in input.mouse.keygroup) {
 		input.mouse.keygroup[key].pressed = false;
 	}
 }
 
-input.mouse.releaseAll = function(){
-	for(var key in input.mouse.keygroup){
+input.mouse.releaseAll = function() {
+	for(var key in input.mouse.keygroup) {
 		input.mouse.keygroup[key].pressed = false;
 	}
 }
 
-input.mouse.check = function(name){
-	if(input.mouse.keygroup[name]){
+input.mouse.check = function(name) {
+	if(input.mouse.keygroup[name]) {
 		return input.mouse.keygroup[name].pressed
 	}
 }
 
-input.mouse.checkAny = function(){
-	for(var key in input.mouse.keygroup){
-		if(input.mouse.keygroup[key].pressed){
+input.mouse.checkAny = function() {
+	for(var key in input.mouse.keygroup) {
+		if(input.mouse.keygroup[key].pressed) {
 			return mouseKey.keycode
 		}
 	}
 	return -1
 }
 
-input.mouse.checkKeycode = function(){
+input.mouse.checkKeycode = function() {
 	return lastKeycode
 }
 
-input.mouse.getClickedPosition = function(){
-	return {x:input.mouse.pressedX, y:input.mouse.pressedY}
+input.mouse.getClickedPosition = function() {
+	return {
+		x: input.mouse.pressedX,
+		y: input.mouse.pressedY
+	}
 }
 
-input.mouse.getPosition = function(){
-	return {x:input.mouse.x,y:input.mouse.y}
+input.mouse.getPosition = function() {
+	return {
+		x: input.mouse.x,
+		y: input.mouse.y
+	}
 }
 
-input.movementHandle = function(){
+input.movementHandle = function() {
 	input.mouse.movementX = 0;
 	input.mouse.movementY = 0;
 }
 
-window.onmousedown = function(e){
+window.onmousedown = function(e) {
 	var canvas = document.getElementById("canvas-game");
 	var rect = canvas.getBoundingClientRect();
 
 	input.mouse.pressedX = e.clientX - rect.left;
 	input.mouse.pressedY = e.clientY - rect.top;
 
-	if(input.mouse.x < canvas.width && input.mouse.x > 0 && input.mouse.y < canvas.height && input.mouse.y > 0){
+	if(input.mouse.x < canvas.width && input.mouse.x > 0 && input.mouse.y < canvas.height && input.mouse.y > 0) {
 		e.preventDefault();
 	}
 
 	input.mouse.press(input.mouse.pressedX, input.mouse.pressedY, e.button);
 
-	if(input.mouse.debug === true){
-		console.log("button:" +e.button +"|| x:" +input.mouse.pressedX +" || y:" +input.mouse.pressedY);
+	if(input.mouse.debug === true) {
+		console.log("button:" + e.button + "|| x:" + input.mouse.pressedX + " || y:" + input.mouse.pressedY);
 	}
 }
 
-window.onmouseup = function(e){
+window.onmouseup = function(e) {
 	input.mouse.release();
 }
 
-window.onmousemove = function(e){
+window.onmousemove = function(e) {
 	var canvas = document.getElementById("canvas-game");
 	var rect = canvas.getBoundingClientRect();
 	input.mouse.movedThisFrame = true;
 
 	input.mouse.x = e.clientX - rect.left;
 	input.mouse.y = e.clientY - rect.top;
-	
-	if(document.pointerLockElement){
+
+	if(document.pointerLockElement) {
 		input.mouse.movementX = input.mouse.x - input.mouse.lastX;
 		input.mouse.movementY = input.mouse.y - input.mouse.lastY;
 		input.mouse.lastX = input.mouse.x;
 		input.mouse.lastY = input.mouse.y;
 
-	} else{
+	} else {
 		input.mouse.movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
 		input.mouse.movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0;
 	}
 
 	var pressed = false;
-	for(var key in input.mouse.keygroup){
-		if(input.mouse.keygroup[key].pressed === true){
+	for(var key in input.mouse.keygroup) {
+		if(input.mouse.keygroup[key].pressed === true) {
 			pressed = true;
 			break
 		}
 	}
 
-	if(pressed === true){
+	if(pressed === true) {
 		input.mouse.pressedX = input.mouse.x;
 		input.mouse.pressedY = input.mouse.y;
 	}
@@ -157,16 +163,16 @@ window.onmousemove = function(e){
 }
 
 /*
-	88      a8P                           88                                                         88  
-	88    ,88'                            88                                                         88  
-	88  ,88"                              88                                                         88  
-	88,d88'       ,adPPYba,  8b       d8  88,dPPYba,    ,adPPYba,   ,adPPYYba,  8b,dPPYba,   ,adPPYb,88  
-	8888"88,     a8P_____88  `8b     d8'  88P'    "8a  a8"     "8a  ""     `Y8  88P'   "Y8  a8"    `Y88  
-	88P   Y8b    8PP"""""""   `8b   d8'   88       d8  8b       d8  ,adPPPPP88  88          8b       88  
-	88     "88,  "8b,   ,aa    `8b,d8'    88b,   ,a8"  "8a,   ,a8"  88,    ,88  88          "8a,   ,d88  
-	88       Y8b  `"Ybbd8"'      Y88'     8Y"Ybbd8"'    `"YbbdP"'   `"8bbdP"Y8  88           `"8bbdP"Y8  
-	                             d8'                                                                     
-	                            d8'                                                                      
+	88      a8P                           88                                                         88
+	88    ,88'                            88                                                         88
+	88  ,88"                              88                                                         88
+	88,d88'       ,adPPYba,  8b       d8  88,dPPYba,    ,adPPYba,   ,adPPYYba,  8b,dPPYba,   ,adPPYb,88
+	8888"88,     a8P_____88  `8b     d8'  88P'    "8a  a8"     "8a  ""     `Y8  88P'   "Y8  a8"    `Y88
+	88P   Y8b    8PP"""""""   `8b   d8'   88       d8  8b       d8  ,adPPPPP88  88          8b       88
+	88     "88,  "8b,   ,aa    `8b,d8'    88b,   ,a8"  "8a,   ,a8"  88,    ,88  88          "8a,   ,d88
+	88       Y8b  `"Ybbd8"'      Y88'     8Y"Ybbd8"'    `"YbbdP"'   `"8bbdP"Y8  88           `"8bbdP"Y8
+	                             d8'
+	                            d8'
 */
 
 input.keys = {};
@@ -174,33 +180,33 @@ input.keys.keygroup = {};
 input.keys.lastKeycode = 0;
 input.keys.debug = false;
 
-input.keys.addKey = function(name, keycode){
-	if(!input.keys.keygroup[name]){
+input.keys.addKey = function(name, keycode) {
+	if(!input.keys.keygroup[name]) {
 		key = [];
 		key.name = name;
 		key.keycode = [];
 		key.keycode[1] = keycode;
 		key.pressed = false;
 		input.keys.keygroup[key.name] = key;
-	}else{
-		for(var code in input.keys.keygroup[name].keycode){
+	} else {
+		for(var code in input.keys.keygroup[name].keycode) {
 			if(input.keys.keygroup[name].keycode[code] == keycode) return;
 		}
 		input.keys.keygroup[name].keycode[input.keys.keygroup[name].keycode.length + 1] = keycode;
 	}
 }
 
-input.keys.popKey = function(name){
-	if(input.keys.keygroup[name]){
+input.keys.popKey = function(name) {
+	if(input.keys.keygroup[name]) {
 		input.keys.keygroup[name] = null;
 	}
 }
 
-input.keys.press = function(keycode){
+input.keys.press = function(keycode) {
 	input.keys.lastKeycode = keycode;
-	for(var key in input.keys.keygroup){
-		for(var code in input.keys.keygroup[key].keycode){
-			if(input.keys.keygroup[key].keycode[code] == keycode){
+	for(var key in input.keys.keygroup) {
+		for(var code in input.keys.keygroup[key].keycode) {
+			if(input.keys.keygroup[key].keycode[code] == keycode) {
 				input.keys.keygroup[key].pressed = true;
 				return
 			}
@@ -208,10 +214,10 @@ input.keys.press = function(keycode){
 	}
 }
 
-input.keys.release = function(keycode){
-	for(var key in input.keys.keygroup){
-		for(var code in input.keys.keygroup[key].keycode){
-			if(input.keys.keygroup[key].keycode[code] == keycode){
+input.keys.release = function(keycode) {
+	for(var key in input.keys.keygroup) {
+		for(var code in input.keys.keygroup[key].keycode) {
+			if(input.keys.keygroup[key].keycode[code] == keycode) {
 				input.keys.keygroup[key].pressed = false;
 				return
 			}
@@ -219,64 +225,66 @@ input.keys.release = function(keycode){
 	}
 }
 
-input.keys.releaseAll = function(){
-	for(var key in input.keys.keygroup){
+input.keys.releaseAll = function() {
+	for(var key in input.keys.keygroup) {
 		input.keys.keygroup[key].pressed = false;
 	}
 }
 
-input.keys.check = function(name){
-	if(input.keys.keygroup[name]){
+input.keys.check = function(name) {
+	if(input.keys.keygroup[name]) {
 		return input.keys.keygroup[name].pressed;
 	}
 }
 
-input.keys.checkAny = function(){
-	for(var key in input.keys.keygroup){
-		if(input.keys.keygroup[key].pressed){
+input.keys.checkAny = function() {
+	for(var key in input.keys.keygroup) {
+		if(input.keys.keygroup[key].pressed) {
 			return true
 		}
 	}
 	return false
 }
 
-input.keys.checkKeycode = function(){
+input.keys.checkKeycode = function() {
 	return input.keys.lastKeycode;
 }
 
-input.keys.getPressedNum = function(){
+input.keys.getPressedNum = function() {
 	count = 0;
-	for(var key in input.keys.keygroup){
+	for(var key in input.keys.keygroup) {
 		if(input.keys.keygroup[key].pressed == true) count += 1;
 	}
 	return count
-}	
+}
 
-input.keys.scope = function(name){
-	if(input.keys.keygroup[name]){
+input.keys.scope = function(name) {
+	if(input.keys.keygroup[name]) {
 		return true
-	}else{
+	} else {
 		return false
 	}
 }
 
-document.addEventListener("keydown", function(e){
+document.addEventListener("keydown", function(e) {
 	input.keys.press(e.keyCode);
 	if(input.keys.debug) console.log(e.keyCode);
 });
 
-document.addEventListener("keyup", function(e){input.keys.release(e.keyCode)});
+document.addEventListener("keyup", function(e) {
+	input.keys.release(e.keyCode)
+});
 
-window.addEventListener("keydown", function(e){
-    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1){
-        e.preventDefault();
-    }
+window.addEventListener("keydown", function(e) {
+	if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+		e.preventDefault();
+	}
 }, false);
 
-window.onfocus = function(e){
+window.onfocus = function(e) {
 	input.focus = true;
 }
 
-window.onblur = function(e){
+window.onblur = function(e) {
 	input.focus = false;
 }
--- canvas.utils.js
+++ canvas.utils.js
@@ -1,95 +1,94 @@
-
 /*
-	88b           d88                       88           
-	888b         d888                ,d     88           
-	88`8b       d8'88                88     88           
-	88 `8b     d8' 88  ,adPPYYba,  MM88MMM  88,dPPYba,   
-	88  `8b   d8'  88  ""     `Y8    88     88P'    "8a  
-	88   `8b d8'   88  ,adPPPPP88    88     88       88  
-	88    `888'    88  88,    ,88    88,    88       88  
-	88     `8'     88  `"8bbdP"Y8    "Y888  88       88  
+	88b           d88                       88
+	888b         d888                ,d     88
+	88`8b       d8'88                88     88
+	88 `8b     d8' 88  ,adPPYYba,  MM88MMM  88,dPPYba,
+	88  `8b   d8'  88  ""     `Y8    88     88P'    "8a
+	88   `8b d8'   88  ,adPPPPP88    88     88       88
+	88    `888'    88  88,    ,88    88,    88       88
+	88     `8'     88  `"8bbdP"Y8    "Y888  88       88
 */
 
 var numArray = [-1, 1];
 Math.PI_2 = Math.PI / 2;
 Math.TAU = Math.PI * 2;
 
-Math.randomRange = function(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+Math.randomRange = function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-Math.randNonNull = function(){
-	return numArray[Math.randomRange(0,1)];
+Math.randNonNull = function() {
+	return numArray[Math.randomRange(0, 1)];
 }
 
-Math.randomRangeFloat = function(a, b, decimal){
+Math.randomRangeFloat = function(a, b, decimal) {
 	var value_a = 1;
 	var value_b = 1;
 
-	if(a != 0){
+	if(a != 0) {
 		value_a = a / a;
-	}else{
+	} else {
 		value_a = 1;
 	}
 
-	if(b != 0){
+	if(b != 0) {
 		value_b = b / b;
-	}else{
+	} else {
 		value_b = 1;
 	}
 
-	if(decimal){
-		return Math.randomRange(a * Math.pow(10,decimal) * value_a, b * (10 * decimal) * value_b) / Math.pow(10,decimal);
-	}else{
-		return Math.randomRange(a * Math.pow(10,5) * value_a, b * Math.pow(10,5) * value_b) / Math.pow(10,5);
+	if(decimal) {
+		return Math.randomRange(a * Math.pow(10, decimal) * value_a, b * (10 * decimal) * value_b) / Math.pow(10, decimal);
+	} else {
+		return Math.randomRange(a * Math.pow(10, 5) * value_a, b * Math.pow(10, 5) * value_b) / Math.pow(10, 5);
 	}
 }
 
-Math.round = function(num, idp){
-	var mult = Math.pow(10,(idp || 0));
+Math.round = function(num, idp) {
+	var mult = Math.pow(10, (idp || 0));
 	return Math.floor(num * mult + 0.5) / mult;
 }
 
-Math.clamp = function(num, min, max){
+Math.clamp = function(num, min, max) {
 	return Math.max(min, Math.min(num, max));
 }
 
-Math.cycle = function(num, min, max){
+Math.cycle = function(num, min, max) {
 	var tick = 0;
 
-	while(num < min || num > max && tick < 10){
+	while(num < min || num > max && tick < 10) {
 		tick++;
 
-		if(num < min && min < max){
+		if(num < min && min < max) {
 			var dist = min - num;
 			num = max - dist;
-		}else if(num > max && max > min){
+		} else if(num > max && max > min) {
 			var dist = max - num;
 			num = min + dist;
 		}
 
-		if(tick == 10){
+		if(tick == 10) {
 			return Math.clamp(num, min, max);
 		}
 	}
 	return num;
 }
 
-Math.lerp = function(num, endNum, t){
+Math.lerp = function(num, endNum, t) {
 	return num + (endNum - num) * t;
 }
 
-Math.step = function(num, endNum, s){
-	if(endNum < num){
+Math.step = function(num, endNum, s) {
+	if(endNum < num) {
 		num = Math.max(num - s, endNum);
-	}else{
+	} else {
 		num = Math.min(num + s, endNum);
 	}
 	return num
 }
 
-Math.percent = function(startNum, endNum, percent){
-	if(percent > 100 || percent < 0){
+Math.percent = function(startNum, endNum, percent) {
+	if(percent > 100 || percent < 0) {
 		percent = Math.clamp(percent, 0, 100);
 	}
 
@@ -98,65 +97,65 @@ Math.percent = function(startNum, endNum, percent){
 	return num
 }
 
-Math.lerpDeg = function(angle, endAngle, t){
+Math.lerpDeg = function(angle, endAngle, t) {
 	var difference = Math.abs(endAngle - angle);
 
-    if(difference > 180){
-        if(endAngle > angle){
-            angle += 360;
-        }else{
-        	endAngle += 360;
-        }
-    }
+	if(difference > 180) {
+		if(endAngle > angle) {
+			angle += 360;
+		} else {
+			endAngle += 360;
+		}
+	}
 
 	var value = angle + (endAngle - angle) * t;
 	var rangeZero = 360;
 
-	if(value >= 0 && value <= 360){
+	if(value >= 0 && value <= 360) {
 		return value;
-	}else{
+	} else {
 		return value % rangeZero;
 	}
 }
 
-Math.lerpRad = function(angle, endAngle, t){
+Math.lerpRad = function(angle, endAngle, t) {
 	var difference = Math.abs(endAngle - angle)
 
-    if(difference > Math.PI){
-        if(endAngle > angle){
-            angle = angle + (Math.PI * 2);
-        }else{
-        	endAngle = endAngle + (Math.PI * 2);
-        }
-    }
+	if(difference > Math.PI) {
+		if(endAngle > angle) {
+			angle = angle + (Math.PI * 2);
+		} else {
+			endAngle = endAngle + (Math.PI * 2);
+		}
+	}
 
 	var value = angle + (endAngle - angle) * t;
 	var rangeZero = (Math.PI * 2);
 
-	if(value >= 0 && value <= (Math.PI * 2)){
+	if(value >= 0 && value <= (Math.PI * 2)) {
 		return value;
-	}else{
+	} else {
 		return value % rangeZero;
 	}
 }
 
-Math.deg = function(rad){
- 	return rad * (180 / Math.PI);
+Math.deg = function(rad) {
+	return rad * (180 / Math.PI);
 }
 
-Math.rad = function(deg){
+Math.rad = function(deg) {
 	return deg * (Math.PI / 180);
 }
 
 /*
-	88888888888           88  88                                                                          
-	88                    88  88                                                                          
-	88                    88  88                                                                          
-	88aaaaa  88       88  88  88  ,adPPYba,   ,adPPYba,  8b,dPPYba,   ,adPPYba,   ,adPPYba,  8b,dPPYba,   
-	88"""""  88       88  88  88  I8[    ""  a8"     ""  88P'   "Y8  a8P_____88  a8P_____88  88P'   `"8a  
-	88       88       88  88  88   `"Y8ba,   8b          88          8PP"""""""  8PP"""""""  88       88  
-	88       "8a,   ,a88  88  88  aa    ]8I  "8a,   ,aa  88          "8b,   ,aa  "8b,   ,aa  88       88  
-	88        `"YbbdP'Y8  88  88  `"YbbdP"'   `"Ybbd8"'  88           `"Ybbd8"'   `"Ybbd8"'  88       88  
+	88888888888           88  88
+	88                    88  88
+	88                    88  88
+	88aaaaa  88       88  88  88  ,adPPYba,   ,adPPYba,  8b,dPPYba,   ,adPPYba,   ,adPPYba,  8b,dPPYba,
+	88"""""  88       88  88  88  I8[    ""  a8"     ""  88P'   "Y8  a8P_____88  a8P_____88  88P'   `"8a
+	88       88       88  88  88   `"Y8ba,   8b          88          8PP"""""""  8PP"""""""  88       88
+	88       "8a,   ,a88  88  88  aa    ]8I  "8a,   ,aa  88          "8b,   ,aa  "8b,   ,aa  88       88
+	88        `"YbbdP'Y8  88  88  `"YbbdP"'   `"Ybbd8"'  88           `"Ybbd8"'   `"Ybbd8"'  88       88
 */
 
 var fullscreen = {};
@@ -165,38 +164,38 @@ fullscreen.currentCallback;
 fullscreen.pointerlock = false;
 fullscreen.debug = false;
 
-fullscreen.available = function(){
-	if(document.documentElement.requestFullscreen || document.documentElement.mozCancelFullScreen || document.documentElement.webkitRequestFullscreen || document.documentElement.msRequestFullscreen){
+fullscreen.available = function() {
+	if(document.documentElement.requestFullscreen || document.documentElement.mozCancelFullScreen || document.documentElement.webkitRequestFullscreen || document.documentElement.msRequestFullscreen) {
 		return true;
-	} else{
+	} else {
 		return false;
 	}
 }
 
-fullscreen.activated = function(){
-	if(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement){
+fullscreen.activated = function() {
+	if(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
 		return true;
-	} else{
+	} else {
 		return false;
 	}
 }
 
-fullscreen.request = function(element){
+fullscreen.request = function(element) {
 	element = element || document.documentElement;
-	if(element.requestFullscreen){
+	if(element.requestFullscreen) {
 		element.requestFullscreen();
-	} else if(element.mozRequestFullScreen){
+	} else if(element.mozRequestFullScreen) {
 		element.mozRequestFullScreen();
-	} else if(element.webkitRequestFullscreen){
+	} else if(element.webkitRequestFullscreen) {
 		element.webkitRequestFullscreen();
-	} else if(element.msRequestFullscreen){
+	} else if(element.msRequestFullscreen) {
 		element.msRequestFullscreen();
 	}
 
 	currentElement = element;
 }
 
-fullscreen.cancel = function(){
+fullscreen.cancel = function() {
 	if(document.exitFullscreen) {
 		document.exitFullscreen();
 	} else if(document.mozCancelFullScreen) {
@@ -209,37 +208,37 @@ fullscreen.cancel = function(){
 	if(fullscreen.currentCallback) fullscreen.currentCallback(false);
 }
 
-fullscreen.toggle = function(element, pointerlock, callback){
-	if(fullscreen.available() == true){
-		if(fullscreen.activated() == false){
+fullscreen.toggle = function(element, pointerlock, callback) {
+	if(fullscreen.available() == true) {
+		if(fullscreen.activated() == false) {
 			fullscreen.request(element);
 
 			pointerlock = pointerlock || false;
 
-			if(pointerlock === true){
-				if(document.webkitFullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element){ 
+			if(pointerlock === true) {
+				if(document.webkitFullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element) {
 					element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
 					element.requestPointerLock();
 				}
 			}
 
-			if(callback){
+			if(callback) {
 				fullscreen.currentCallback = callback;
 			}
-		} else{
+		} else {
 			fullscreen.cancel();
 
-			if(callback){
+			if(callback) {
 				fullscreen.currentCallback = null;
 			}
 		}
 	}
 }
 
-fullscreen.changeCallback = function(){
-	if(document.webkitFullscreenElement || document.mozFullscreenElement || document.mozFullScreenElement){
+fullscreen.changeCallback = function() {
+	if(document.webkitFullscreenElement || document.mozFullscreenElement || document.mozFullScreenElement) {
 		if(fullscreen.currentCallback) fullscreen.currentCallback(true);
-	} else{
+	} else {
 		if(fullscreen.currentCallback) fullscreen.currentCallback(false);
 	}
 }
@@ -248,11 +247,11 @@ document.addEventListener('fullscreenchange', fullscreen.changeCallback, false);
 document.addEventListener('mozfullscreenchange', fullscreen.changeCallback, false);
 document.addEventListener('webkitfullscreenchange', fullscreen.changeCallback, false);
 
-function pointerLockChange(){
-	if(fullscreen.debug === true){
+function pointerLockChange() {
+	if(fullscreen.debug === true) {
 		if(document.mozPointerLockElement || document.webkitPointerLockElement) {
 			console.log("Pointer Lock was successful.");
-		} else{
+		} else {
 			console.log("Pointer Lock was lost.");
 		}
 	}
@@ -262,8 +261,8 @@ document.addEventListener('pointerlockchange', pointerLockChange, false);
 document.addEventListener('mozpointerlockchange', pointerLockChange, false);
 document.addEventListener('webkitpointerlockchange', pointerLockChange, false);
 
-function pointerLockError(){
-	if(fullscreen.debug === true){
+function pointerLockError() {
+	if(fullscreen.debug === true) {
 		console.log("Error while locking pointer.");
 	}
 }
@@ -273,19 +272,19 @@ document.addEventListener('mozpointerlockerror', pointerLockError, false);
 document.addEventListener('webkitpointerlockerror', pointerLockError, false);
 
 /*
-	  ,ad8888ba,                88  88  88             88                            
-	 d8"'    `"8b               88  88  ""             ""                            
-	d8'                         88  88                                               
-	88              ,adPPYba,   88  88  88  ,adPPYba,  88   ,adPPYba,   8b,dPPYba,   
-	88             a8"     "8a  88  88  88  I8[    ""  88  a8"     "8a  88P'   `"8a  
-	Y8,            8b       d8  88  88  88   `"Y8ba,   88  8b       d8  88       88  
-	 Y8a.    .a8P  "8a,   ,a8"  88  88  88  aa    ]8I  88  "8a,   ,a8"  88       88  
-	  `"Y8888Y"'    `"YbbdP"'   88  88  88  `"YbbdP"'  88   `"YbbdP"'   88       88  
+	  ,ad8888ba,                88  88  88             88
+	 d8"'    `"8b               88  88  ""             ""
+	d8'                         88  88
+	88              ,adPPYba,   88  88  88  ,adPPYba,  88   ,adPPYba,   8b,dPPYba,
+	88             a8"     "8a  88  88  88  I8[    ""  88  a8"     "8a  88P'   `"8a
+	Y8,            8b       d8  88  88  88   `"Y8ba,   88  8b       d8  88       88
+	 Y8a.    .a8P  "8a,   ,a8"  88  88  88  aa    ]8I  88  "8a,   ,a8"  88       88
+	  `"Y8888Y"'    `"YbbdP"'   88  88  88  `"YbbdP"'  88   `"YbbdP"'   88       88
 */
 
 var collision = {};
 
-collision.box = function(ax1, ay1, az1, aw, ah, ad, bx1, by1, bz1, bw, bh, bd){
+collision.box = function(ax1, ay1, az1, aw, ah, ad, bx1, by1, bz1, bw, bh, bd) {
 	var ax2 = ax1 + aw;
 	var ay2 = ay1 + ah;
 	var az2 = az1 + ad;
@@ -293,16 +292,16 @@ collision.box = function(ax1, ay1, az1, aw, ah, ad, bx1, by1, bz1, bw, bh, bd){
 	var by2 = by1 + bh;
 	var bz2 = bz1 + bd;
 
-	return (ax1 < bx2 &&
-			ax2 > bx1 &&
-			ay1 < by2 &&
-			ay2 > by1 &&
-			az1 < bz2 &&
-			az2 > bz1
+	return(ax1 < bx2 &&
+		ax2 > bx1 &&
+		ay1 < by2 &&
+		ay2 > by1 &&
+		az1 < bz2 &&
+		az2 > bz1
 	);
 }
 
-collision.boxHelper = function(aPosition, aScale, bPosition, bScale){
+collision.boxHelper = function(aPosition, aScale, bPosition, bScale) {
 	return(
 		collision.box(
 			aPosition.x - aScale.x / 2,
